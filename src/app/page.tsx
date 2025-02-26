bc06697964f958f925e273618f2ec4746dba4963
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-02-26 12:16:19 +0100

    Criado component inpur focus quando tiver o mouse sobre ela e digitando e com o atributo error para mostrar a borda vermelha e o icon que é opcional. Foi feita ainstalação e configuração do Biome, que é um formatador de codigo mais rapido que o prittier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/button";
 import { IconButton } from "@/components/icon-button";
-import { ArrowRight, Copy } from "lucide-react";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail } from "lucide-react";
 
 export default function Home() {
   return (
@@ -11,6 +12,12 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+      <InputRoot>
+        <InputIcon>
+          <Mail className="size-5" />
+        </InputIcon>
+        <InputField />
+      </InputRoot>
     </main>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,12 @@
-import { ComponentProps } from "react";
-import { twMerge } from "tailwind-merge";
+import type { ComponentProps } from 'react'
 
-interface ButtonProps extends ComponentProps<"button"> {}
+interface ButtonProps extends ComponentProps<'button'> {}
 
 export function Button(props: ButtonProps) {
   return (
     <button
       className="flex justify-between items-center p-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
       {...props}
-    ></button>
-  );
+    />
+  )
 }
