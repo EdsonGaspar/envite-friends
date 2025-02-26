8c0c9d37f8864f7f6205c829be5283a796cc85fd
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-02-26 16:22:52 +0100

    Criado a pagina home, aproveitando todos os componentes previamente criados.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Montserrat, Oxanium } from "next/font/google";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "Envite Friends",
-  description: "Send links to friends for envite them.",
-};
+  title: 'Envite Friends',
+  description: 'Send links to friends for envite them.',
+}
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "600"],
-  variable: "--font-montserrat",
-});
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-montserrat',
+})
 
 const oxanium = Oxanium({
-  subsets: ["latin"],
-  weight: ["500", "600"],
-  variable: "--font-oxanium",
-});
+  subsets: ['latin'],
+  weight: ['500', '600'],
+  variable: '--font-oxanium',
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt" className={`${montserrat.variable} ${oxanium.variable}`}>
-      <body className="bg-gray-900 text-gray-100 antialiased">{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/Background.png)] bg-no-repeat bg-top md:bg-top-right bg-cover">
+        {children}
+      </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,70 @@
-import { Button } from "@/components/button";
-import { IconButton } from "@/components/icon-button";
-import { InputField, InputIcon, InputRoot } from "@/components/input";
-import { ArrowRight, Copy, Mail } from "lucide-react";
-
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { ArrowRight, Mail, Radio, User } from 'lucide-react'
+import Image from 'next/image'
+import logo from '../assets/Logo.svg'
 export default function Home() {
   return (
-    <main>
-      <Button>
-        Something like that <ArrowRight />{" "}
-      </Button>
-      <IconButton>
-        <Copy />
-      </IconButton>
-      <InputRoot>
-        <InputIcon>
-          <Mail className="size-5" />
-        </InputIcon>
-        <InputField />
-      </InputRoot>
+    <main className="max-w-[1240px] mx-auto ">
+      <div className="min-h-dvh flex flex-col md:justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center md:items-start">
+          <Image
+            src={logo}
+            alt="Envite Friends logo"
+            width={108.5}
+            height={30}
+          />
+          <h1 className="text-4xl text-center font-heading font-medium flex flex-col md:text-7xl md:text-left">
+            <span className="text-blue">CodeCraft</span>
+            Summit 2025
+          </h1>
+        </div>
+
+        <div className="flex flex-col gap-5  items-stretch md:flex-row">
+          <div className="flex-auto bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 ">
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading text-xl font-semibold text-gray-200">
+                Sobre o evento
+              </h2>
+              <span className="text-purple uppercase font-semibold flex items-center gap-2 text-xs">
+                <Radio className="size-5" /> AO VIVO{' '}
+              </span>
+            </div>
+            <p className="text-gray-300 text-sm leading-relaxed md:text-base">
+              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento. Vamos
+              mergulhar nas tendências mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, workshops e hackathons.
+              <br />
+              <br />
+              Dias 15 a 17 de março | Das 18h às 21h
+            </p>
+          </div>
+          <form className="flex flex-col bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full maxw-w-[440px] ">
+            <h2 className="font-heading text-xl font-medium text-gray-200">
+              Inscrição
+            </h2>
+            <div className="space-y-3">
+              <InputRoot>
+                <InputIcon>
+                  <User />
+                </InputIcon>
+                <InputField type="text" placeholder="Nome completo" />
+              </InputRoot>
+              <InputRoot>
+                <InputIcon>
+                  <Mail />
+                </InputIcon>
+                <InputField type="email" placeholder="Insira seu e-mail" />
+              </InputRoot>
+            </div>
+            <IconButton type="submit">
+              Confirmar <ArrowRight />
+            </IconButton>
+          </form>
+        </div>
+      </div>
     </main>
-  );
+  )
 }
